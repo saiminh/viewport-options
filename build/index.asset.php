c57d7ca8a73bc1a3c8bea7485f28850d07dff491
saiminh <saiminh@gmail.com>
2023-04-11 15:29:39 +0200

    adjust for fixed header

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '78daacc0aedb9126bff2');
+<?php return array('dependencies' => array('wp-element'), 'version' => '5adfc65d24a9028d0e17');
