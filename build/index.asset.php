cb46eaf585210d14b4c7d9a7da0487185ff48bf4
saiminh <saiminh@gmail.com>
2023-04-13 00:00:21 +0200

    minify

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '59f9dab8c9716d7d0f29');
+<?php return array('dependencies' => array('wp-element'), 'version' => '56bcc2058fda2543433a');
