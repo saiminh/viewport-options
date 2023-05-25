80b33a28bf840a5190da75f7cf12e6d13ef530a8
saiminh <saiminh@gmail.com>
2023-05-25 12:10:15 +0200

    0px

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '0311d114c259f95f084a');
+<?php return array('dependencies' => array('wp-element'), 'version' => '45b4f30c608845ccdbb2');
