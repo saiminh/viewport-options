e14b79f0894766e4ce5747d18652bf7dc97c04b5
saiminh <saiminh@gmail.com>
2023-05-25 16:23:28 +0200

    important

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '794cf1f1a367ea80339c');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'c69b80d43ee2e79d5a1e');
